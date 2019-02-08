bebc2b2746d880846e97251cf67c47888b653273
murgic <e.murgic@go-james.com>
2019-02-08 08:20:52 +0100

    making progress

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/")
+     * @return Response
+     */
+    public function homepage()
+    {
+        return new Response('My first page!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: "%s"',
+            $slug
+        ));
+    }
+}
