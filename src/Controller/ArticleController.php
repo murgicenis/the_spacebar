b99964abc475b5bc082ef231622b311c7e8c0c45
murgic <e.murgic@go-james.com>
2019-02-08 14:31:39 +0100

    twig install

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -22,9 +22,19 @@ class ArticleController extends AbstractController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show the article: "%s"',
-            $slug
-        ));
+//        return new Response(sprintf(
+//            'Future page to show the article: "%s"',
+//            $slug
+//        ));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 }
